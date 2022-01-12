95a1cc5b9f58486af909809f40000e5f1bb6f60b
Kurian Benoy <70306694+kurianbenoy-aot@users.noreply.github.com>
2022-01-12 10:17:16 +0530

    Update saml_auth.py

--- redash/authentication/saml_auth.py
+++ redash/authentication/saml_auth.py
@@ -85,6 +85,9 @@ def get_saml_client(org):
 
         saml_settings["metadata"] = {"inline": [metadata_inline]}
 
+    if entity_id is not None and entity_id != "":
+        saml_settings["entityid"] = entity_id
+
     if acs_url is not None and acs_url != "":
         saml_settings["entityid"] = acs_url
 
